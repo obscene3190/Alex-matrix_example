36ee16c485e08a4a5d79ff6eabadd6aadb240d57
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-03 23:37:57 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -102,8 +102,36 @@ TEST_CASE("mul matrix")
     std::istringstream istream2{ input2 };
     matrix1.read( istream1 );
     matrix2.read( istream2 );
-    result=matrix1+matrix2;
+    result = matrix1 * matrix2;
     std::ostringstream ostream;
     result.write( ostream );
+    REQUIRE( resultadd == ostream.str() );
+}
+
+TEST_CASE("addequals matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input2{
+        "3, 3\n"
+        "2 2 2\n"
+        "2 2 2\n"
+        "2 2 2" };
+    std::string resultadd{
+        "3, 3\n"
+        "3 3 3\n"
+        "3 3 3\n"
+        "3 3 3" };
+    matrix_t matrix1, matrix2;
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    matrix1+=matrix2;
+    std::ostringstream ostream;
+    matrix1.write( ostream );
     REQUIRE( resultadd == ostream.str() );
 }
