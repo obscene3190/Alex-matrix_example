c26a686543c76fc3924e4585043ed8a7e5c123e5
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-03 20:26:56 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -29,3 +29,31 @@ TEST_CASE("reading matrix")
     
     REQUIRE( input == ostream.str() );
 }
+
+TEST_CASE("adding matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input2{
+        "3, 3\n"
+        "2 2 2\n"
+        "2 2 2\n"
+        "2 2 2" };
+    std::string resultadd{
+        "3, 3\n"
+        "3 3 3\n"
+        "3 3 3\n"
+        "3 3 3" };
+    matrix_t matrix1, matrix2, result;
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    result = matrix1 + matrix2;
+    std::ostringstream ostream;
+    result.write( ostream );
+    REQUIRE( resultadd == ostream.str() );
+}
