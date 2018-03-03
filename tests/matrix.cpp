2697c5106d2c7c1adfead82211e3b39da10b5ab4
obscene3190 <32190896+obscene3190@users.noreply.github.com>
2018-03-03 23:31:06 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -52,6 +52,56 @@ TEST_CASE("adding matrix")
     std::istringstream istream2{ input2 };
     matrix1.read( istream1 );
     matrix2.read( istream2 );
+    result = matrix1 + matrix2;
+    std::ostringstream ostream;
+    result.write( ostream );
+    REQUIRE( resultadd == ostream.str() );
+}
+
+TEST_CASE("sub matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input2{
+        "3, 3\n"
+        "2 2 2\n"
+        "2 2 2\n"
+        "2 2 2" };
+    matrix_t matrix1, matrix2, result;
+    std::istringstream istream1{ input2 };
+    std::istringstream istream2{ input1 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
+    result = matrix1 - matrix2;
+    std::ostringstream ostream;
+    result.write( ostream );
+    REQUIRE( input1 == ostream.str() );
+}
+TEST_CASE("adding matrix")
+{
+    std::string input1{
+        "3, 3\n"
+        "1 1 1\n"
+        "1 1 1\n"
+        "1 1 1" };
+    std::string input2{
+        "3, 3\n"
+        "2 2 2\n"
+        "2 2 2\n"
+        "2 2 2" };
+    std::string resultadd{
+        "3, 3\n"
+        "3 3 3\n"
+        "3 3 3\n"
+        "3 3 3" };
+    matrix_t matrix1, matrix2, result;
+    std::istringstream istream1{ input1 };
+    std::istringstream istream2{ input2 };
+    matrix1.read( istream1 );
+    matrix2.read( istream2 );
     result=matrix1+matrix2;
     std::ostringstream ostream;
     result.write( ostream );
